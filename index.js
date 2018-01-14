348a33ac2e12c16c393cc89f3e7e69a74adf7e91
p huang <huang.pien@gmail.com>
2018-01-14 15:37:48 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,34 @@
+var katzDeliLine = []
+
+function takeANumber(katzDeliLine,name){
+
+  katzDeliLine.push(name)
+  
+  return (`Welcome, ${name}. You are number ${katzDeliLine.indexOf(name)+1} in line.`)
+}
+
+function nowServing (katzDeliLine){
+  if (katzDeliLine.length>0){
+  return (`Currently serving ${katzDeliLine[0]}.`)
+  
+  katzDeliLine.splice(0,1)}
+  
+  else {
+    return ('There is nobody waiting to be served!')}
+}
+
+
+function currentLine(katzDeliLine){
+  let i=0; i<katzDeliLine.length 
+  
+  return (`The line is currently: 
+  '${katzDeliLine.indexOf}+1'. [i]`)
+  
+  i++
+  
+  if (katzDeliLine.length=0){
+    return ('The line is currently empty.')
+  }
+}
+
+// iterate through katzDeliLine[i]
