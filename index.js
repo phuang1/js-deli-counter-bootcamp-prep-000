85ec5a2009e80fa8f4094dbdc13aadcad4e2d1d2
p huang <huang.pien@gmail.com>
2018-01-14 15:44:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function nowServing (katzDeliLine){
   if (katzDeliLine.length>0){
   return (`Currently serving ${katzDeliLine.splice(0,1)}.`)}
 
-  else if {katzDeliLine.length===0){
+  else if (katzDeliLine.length===0){
     return ('There is nobody waiting to be served!')}
   }
 
