f4f7b653fe77297f0eb19dc68347539076943231
p huang <huang.pien@gmail.com>
2018-01-14 15:44:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ function takeANumber(katzDeliLine,name){
 
 function nowServing (katzDeliLine){
   if (katzDeliLine.length>0){
-  return (`Currently serving ${katzDeliLine.splice(0,1)}.`)
+  return (`Currently serving ${katzDeliLine.splice(0,1)}.`)}
 
-  else {
+  else if {katzDeliLine.length===0){
     return ('There is nobody waiting to be served!')}
-}
+  }
 
 
 function currentLine(katzDeliLine){
