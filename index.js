34a00f8a8cf8bbbc7e44c5fde5e691948e0460f6
p huang <huang.pien@gmail.com>
2018-01-14 16:16:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,14 +18,16 @@ function nowServing (katzDeliLine){
 
 function currentLine(katzDeliLine){
   if (katzDeliLine.length===0){
-    return ('The line is currently empty.')}
+    return ('The line is currently empty.')
+  }
     
   var numbersNames = []
   
   for (let i=0; i<katzDeliLine.length; i++) { 
-  numbersNames.push(`${i + 1}. ${katzDeliLine[i]}`)}
+  numbersNames.push(`${i + 1}. ${katzDeliLine[i]}`)
+  }
   
   return `The line is currently: 
-  ${numbersNamesString}`}
+  ${numbersNames.join}`}
   
 }
