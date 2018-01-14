0e04622935afa0e4e8338914165e7fbc71674415
p huang <huang.pien@gmail.com>
2018-01-14 16:09:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ function currentLine(katzDeliLine){
   numbersNames.push(`${i + 1}. ${katzDeliLine[i]}`)
   
   return `The line is currently: 
-  ${numbersNames.join()}`}
+  ${numbersNames.join(', ')}`}
   
 }
